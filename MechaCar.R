45f24cd8ff39c43e1b7cac521632b3ec46a6bb77
Scoakes <121137779+Scoakes@users.noreply.github.com>
2023-04-16 16:28:19 -0400

    Add files via upload

---
+++ MechaCar.R
@@ -0,0 +1,15 @@
+library(dbplyr)
+MechaCar <- read.csv(file = '/Users/christopheroakes/Documents/Coding Bootcamp/Module 16 - R/Challenge/Starter_Code-5/MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar) #generate multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar)) #generate summary statistics
+
+
+SusCoil <- read.csv(file = '/Users/christopheroakes/Documents/Coding Bootcamp/Module 16 - R/Challenge/Starter_Code-5/Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+total_summary <- SusCoil %>% summarize(mean(PSI),median(PSI),var(PSI), sd(PSI), .groups = 'keep')
+lot_summary <- SusCoil %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI),median(PSI),var(PSI), sd(PSI), .groups= 'keep')
+
+t.test(SusCoil$PSI,mu=1500)
+t.test(subset(SusCoil$PSI,SusCoil$Manufacturing_Lot=='Lot1'),mu=1500)
+t.test(subset(SusCoil$PSI,SusCoil$Manufacturing_Lot=='Lot2'),mu=1500)
+t.test(subset(SusCoil$PSI,SusCoil$Manufacturing_Lot=='Lot3'),mu=1500)
